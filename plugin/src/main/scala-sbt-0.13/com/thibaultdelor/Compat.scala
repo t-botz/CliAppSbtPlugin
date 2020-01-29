74ce344187de87eb34d88fb62d2150002b089179
Tibo <thibault.delor@cba.com.au>
2020-01-29 17:31:39 +1100

    Improve how the classpath for the forked JVM is handled

--- plugin/src/main/scala-sbt-0.13/com/thibaultdelor/Compat.scala
+++
@@ -1,9 +0,0 @@
-package com.thibaultdelor
-
-import sbt.{File, ForkOptions}
-
-object Compat {
-  def forkOptions(bootJars: Seq[File]): ForkOptions = {
-    ForkOptions(bootJars = bootJars)
-  }
-}
--- plugin/src/main/scala/com/thibaultdelor/MyWrapperPlugin.scala
+++ plugin/src/main/scala/com/thibaultdelor/MyWrapperPlugin.scala
@@ -20,19 +20,23 @@ object MyWrapperPlugin extends AutoPlugin {
   override def projectConfigurations: Seq[Configuration] = Seq(CliConfig)
 
   override lazy val projectSettings = Seq(
-    ivyConfigurations += CliConfig,
     runCli := runIt.value,
+    autoScalaLibrary in CliConfig := false,
     libraryDependencies ++= Seq(
+      "org.scala-lang"   % "scala-library"             % "2.11.12"     % CliConfig,
       "com.thibaultdelor" % "mycli_2.11" %  "0.0.1" % CliConfig
     )
   )
 
   lazy val runIt =
     Def.task {
-      val dependencies = (update in CliConfig).value
+      val dependencies = (update in CliConfig).value.select(configurationFilter(CliConfig.name))
       val process = Fork.java.fork(
-        Compat.forkOptions(bootJars = dependencies.allFiles),
-        Seq("com.thibaultdelor.Hello")
+        ForkOptions(),
+        Seq(
+          "-classpath",
+          dependencies.mkString(":"),
+          "com.thibaultdelor.Hello")
       )
       if(process.exitValue() != 0) {
           throw new sbt.MessageOnlyException("Error happened when trying to call the cli")
